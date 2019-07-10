76620ab4ce43e4ad10467858a40938da65fcf17e
alastair38 <alastair@alastaircox.com>
2019-07-10 18:09:28 +0100

    Version 0.88 - refactoring of resources page for local guides

--- parts/loop-resources.php
+++ parts/loop-resources.php
@@ -1,12 +1,40 @@
 
-<article id="post-<?php the_ID(); ?>" class="col s12 m6 resource-article" role="article">
+<article id="post-<?php the_ID(); ?>" class="col s12 m6 l4 resource-article" role="article">
 
-	<section class="col s12 grey lighten-3 center">
-		<h2 class="h5 "><a href="<?php the_permalink() ?>" class="center" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+	<section class="grey lighten-4 card">
+
+		<div class="card-content">
+			<h2 class="card-title"><a href="<?php the_permalink() ?>" class="center" rel="bookmark" ><?php the_title(); ?></a></h2>
+		
+
 
 		<?php
+		if (is_tax( 'resources_category', array( 'local-groups' ) )) {
+
+
+			$field = get_field('section', $post->ID);
+			$activity_details = $field[0]['blocks'][0]['activity_group_details'];
+			$frequency = $activity_details['activity_frequency']['label'];
+			$address = $activity_details['map_address'];
+			$address = explode(",", $address['address']);
+
+			echo '<div class="schedule">';
+
+			if ($frequency == "monthly") {
+				echo $frequency . ' on the ' . $activity_details['activity_frequency_month']['label'] . ' '
+			 . $activity_details['activity_day']['label'];
+		 } else {
+			 echo $frequency . ' every '
+ 		 . $activity_details['activity_day']['label'];
+
+		 }
+		 echo ' at ' . $address[0] . '</div></div>';
+
+
+			echo '<div class="card-action"><i class="material-icons left">info</i>Organised by ' . $activity_details['activity_organiser'] . '</div>';
+		} else {
 		if ($post->post_parent != 0) {
-			echo '<div id="guide-name"><a class="btn-flat" href="' . get_the_permalink($post->post_parent) . '"><i class="material-icons left">library_books</i>From ' . get_the_title($post->post_parent) . ' guide</a></div>';
+			echo '</div><div class="card-action"><i class="material-icons left">library_books</i>Part of ' . get_the_title($post->post_parent) . ' guide</div>';
 		} else {
 			$args = array(
 				'post_parent' => $post->ID,
@@ -19,16 +47,14 @@
 			$total = $count + 1;
 
 
-				echo '<div id="guide-name" class="btn-flat"><i class="material-icons left">format_list_numbered</i>' . $total . '-page guide</div>';
+				echo '</div><div class="card-action"><i class="material-icons left">format_list_numbered</i>' . $total . '-page guide</div>';
 
+		}
 		}
 		//print_R($post);
 
 ?>
 
-	<footer class="card-content" style="position: relative; padding: .5rem 0;">
-			<?php get_template_part( 'parts/content', 'byline' ); ?>
 
-	</footer>
 	</section>
 </article>
