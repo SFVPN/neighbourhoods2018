a37748584b331cd5ba0b459a1cbdfd5121e5101e
alastair38 <alastair@alastaircox.com>
2019-11-13 16:48:59 +0000

    v1.17 bug fix in loop-resources-new.php

--- parts/loop-resources-new.php
+++ parts/loop-resources-new.php
@@ -3,14 +3,14 @@
 
 	<section class="col s12 grey lighten-3">
 		<h2 class="h5 "><a href="<?php the_permalink() ?>" class="center" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-
+		<div class="search-content">
 
 		<?php // check for rows (parent repeater)
 		$meta = get_post_meta($post->ID);
 
 		if( have_rows('section', $post->ID) ): ?>
 
-			<?php echo '<div class="search-content">';
+			<?php
 
 
 			// loop through rows (parent repeater)
@@ -38,6 +38,9 @@
 						endwhile; ?>
 
 					<?php endif; //if( get_sub_field('blocks') ):
+						endwhile;
+
+					 endif; // if( get_field('to-do_lists') ):
 
 							// $activity = get_field('activity_group_details');
 							// $email = $activity['group_email'];
@@ -98,17 +101,23 @@
 
 
 						$details = get_field('group_details');
-						$email = $details['group_email'];
-						if( $email ):
+						//print_R($details);
+						$town = $details['group_address_town'];
+
+						if( $town ):
 
 							//$email = get_sub_field('group_email');
 							$activity_phone = $details['group_phone'];
-							$town = $details['group_address_town'];
+							$email = $details['group_email'];
 							$formatted_phone = explode(" ", $activity_phone);
 							$formatted_phone = implode("-", $formatted_phone);
 
 							echo '<span class="block"><strong>Location</strong> ' . $town . '</span>';
-							echo '<span class="block"><strong>Phone</strong> <a href="tel:' . $formatted_phone . '">' . $activity_phone . '</a></span>';
+							
+							if($activity_phone) {
+								echo '<span class="block"><strong>Phone</strong> <a href="tel:' . $formatted_phone . '">' . $activity_phone . '</a></span>';
+							}
+
 							if($email) {
 								echo '<span class="block"><strong>Email</strong> <a href="mailto:' . $email . '">' . $email . '</a></span>';
 							}
@@ -118,7 +127,7 @@
 						?>
 
 
-			<?php endwhile; // while( has_sub_field('to-do_lists') ):
+			<?php  // while( has_sub_field('to-do_lists') ):
 
 				if ($post->post_parent != 0) {
 					echo '<span class="footer-content"><i class="material-icons left">assignment</i>' . __( 'This is part of the ', 'ocn' ) . '<a href="' . get_the_permalink($post->post_parent) . '">' . get_the_title($post->post_parent) . '</a>' .  __( ' guide', 'ocn' ) . '</span>';
@@ -141,7 +150,6 @@
 				echo '</div >';
 				?>
 
-		<?php endif; // if( get_field('to-do_lists') ): ?>
 
 	<?php //endwhile; // end of the loop. ?>
  	<footer class="card-content">
